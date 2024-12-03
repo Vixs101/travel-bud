f835674fb4406a12756c77df207d85ce6ece84fa
Vixs101 <elijahvix695@gmail.com>
2024-12-03 19:58:28 +0100

    feat: finished the ui of the dashboard nav

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DashboardNav from '@/components/DashboardNav'
+
+export default function dashboard() {
+  return (
+    <section className='h-screen bg-[#F0F0F0] p-3 lg:px-28 w-full'>
+        <DashboardNav/>
+    </section>
+  )
+}
